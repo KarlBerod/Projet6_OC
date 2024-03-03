1dfe115903cab6df52249be33866dc92ec4f9acd
KarlBerod <karl.berod@gmail.com>
2024-03-03 04:14:44 +0100

    finalized project

---
+++ index.js
@@ -0,0 +1,243 @@
+//collecting data from the API
+fetch("http://localhost:5678/api/works")
+  .then(response => response.json())
+  .then(fetchedData => {
+    data = fetchedData;
+    console.log(data);
+    display(data, 0);
+    displayModal(data);
+})
+.catch(error => {
+    console.error('Erreur lors de la récupération des données: ', error);
+});
+
+//for each item collected from the data, a HTML element is created
+function display(data, asked_category){
+    const container= document.getElementById('gallery');
+    container.innerHTML="";
+
+    data.forEach(item => {
+        if(item.categoryId==asked_category || asked_category==0){ //applying item filter
+            const categorie= item.categoryId;
+            const figure= document.createElement("figure");
+            figure.className = `categorie${categorie}`;
+
+            const image=document.createElement("img");
+            image.src = item.imageUrl;
+            image.alt = item.title;
+
+            const figcaption=document.createElement("figcaption");
+            figcaption.innerHTML = item.title;
+
+            figure.appendChild(image);
+            figure.appendChild(figcaption);
+
+            container.appendChild(figure);
+        }
+    });
+}
+
+const category_0 = document.getElementById('button_category_0');
+const category_1 = document.getElementById('button_category_1');
+const category_2 = document.getElementById('button_category_2');
+const category_3 = document.getElementById('button_category_3');
+
+// option to filter categories on button click
+category_0.addEventListener('click', () => {
+    display(data, 0);
+});
+
+category_1.addEventListener('click', () => {
+    display(data, 1);
+});
+
+category_2.addEventListener('click', () => {
+    display(data, 2);
+});
+
+category_3.addEventListener('click', () => {
+    display(data, 3);
+});
+
+//function triggering on the click on "modifier" button to display its gallery in the modal window
+function displayModal(data){
+    const container= document.getElementById('modal-gallery');
+    container.innerHTML="";
+
+    data.forEach(item => {
+        const subContainer = document.createElement("div");
+        subContainer.className="modal-images";
+
+        const trashLogo = document.createElement("i");
+        trashLogo.className=`fa-solid fa-trash-can fa-xs id_${item.id}`;  
+
+        trashLogo.addEventListener('click', function(){
+            deleteItem(item.id);
+        });
+
+        const image=document.createElement("img");
+        image.src = item.imageUrl;
+        image.alt = item.title;
+
+        subContainer.appendChild(image);
+        subContainer.appendChild(trashLogo);
+
+        container.appendChild(subContainer);
+    });
+}
+
+//delete an item from the API when the trash logo is clicked
+function deleteItem(Id){
+    fetch(`http://localhost:5678/api/works/${Id}`, {
+        method: "DELETE",
+        headers: { 
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    })
+    .then(response => {
+        if(response.ok){
+            console.log('Item supprimé');
+        } 
+        else {
+            console.error('Erreur lors de la suppression:', response.statusText);
+        }
+    })
+    .catch(error => console.error('Erreur:', error));
+}
+
+//adds an item in the API if every information is documented
+async function addItem(event){
+    event.preventDefault();
+    event.stopPropagation();
+    const formData = new FormData();
+
+    let formTitle = document.getElementById('title').value;
+    formData.append('title',  formTitle);
+
+    let formImageUrl = document.getElementById('file').files[0];
+    formData.append('image', formImageUrl);
+
+    let formCategoryId = document.getElementById('category').value;
+    formData.append('category', formCategoryId);
+    
+    let response = await fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: { 
+            "Authorization": `Bearer ${token}`
+        },
+        body: formData
+    });
+};
+
+
+//Displays the preview of the image in the form
+document.getElementById('file').addEventListener('change', function(event){
+    const file = event.target.files[0]; 
+    if (file){
+        var fileType = file["type"];
+        var validFileType = ["image/jpeg", "image/png"];
+        //if the file isn't an image or exceeds 4Mo an alert pops up
+        if (!validFileType.includes(fileType) || file.size > 4*1024*1024){
+            alert("Veuillez sélectionner un fichier image valide (JPEG ou PNG) de 4Mo max.");
+            return;
+        }
+
+        var imageReader = new FileReader();
+        imageReader.onload = function(e){
+            var img = document.createElement('img');
+            img.src = e.target.result;
+            var imageForm = document.getElementById('hidden-form');
+            imageForm.classList.add("hidden");
+            var preview = document.getElementById('image-preview');
+            preview.appendChild(img);
+        };
+        imageReader.readAsDataURL(file);
+    }
+});
+
+document.getElementById('modal-form').addEventListener('submit', addItem);
+
+let admin = localStorage.getItem('admin');
+let token = localStorage.getItem('token');
+
+//adds and remove classes from the html to hide and add content related to the administrator
+function adminLoad(){
+    if(admin==1){
+        document.getElementById("admin-header").classList.remove("hidden");
+        document.getElementById("js-modal1").classList.remove("hidden");
+        document.getElementById("categories").classList.add("hidden");
+        document.getElementById("menu-login").classList.add("hidden");
+        document.getElementById("menu-logout").classList.remove("hidden");
+        const categoryButtons=document.querySelectorAll(".category-button");
+        categoryButtons.forEach(function(button){
+            button.classList.add("hidden");
+        });
+    }
+}
+
+//adds and remove classes from the html to hide and add content related to the administrator
+function logout(){
+    localStorage.setItem('admin', '0');
+    document.getElementById("admin-header").classList.add("hidden");
+    document.getElementById("js-modal1").classList.add("hidden");
+    document.getElementById("categories").classList.remove("hidden");
+    document.getElementById("menu-login").classList.remove("hidden");
+    document.getElementById("menu-logout").classList.add("hidden");
+    const categoryButtons=document.querySelectorAll(".category-button");
+    categoryButtons.forEach(function(button){
+        button.classList.remove("hidden");
+    });
+}
+
+document.getElementById("menu-logout").addEventListener("click", logout)
+
+document.addEventListener('DOMContentLoaded', adminLoad)
+
+let modal=null
+
+const openModal = function(event){
+    event.preventDefault()
+    // if exists, close the actual openned modal
+    if (modal !== null){
+        closeModal(event);
+    }
+    // find the parent element if the href is not in the targeted anchor
+    let target = event.target;
+    while(target !== null && !target.href){
+        target = target.parentNode;
+    }
+    //if we find that parent element, we open the new modal related to his href
+    const newModal = document.querySelector(target.getAttribute('href'));
+    if (newModal){
+        newModal.classList.remove("hidden");
+        newModal.removeAttribute('aria-hidden');
+        newModal.setAttribute('aria-modal', 'true');
+        modal = newModal;
+        modal.addEventListener('click', closeModal);
+        modal.querySelector('.close').addEventListener('click', closeModal);
+        modal.querySelector('.modal-wrapper').addEventListener('click', stopPropagation);
+    }
+    
+}
+
+//closing the modal and clearing eventlistener
+const closeModal = function(event){
+    if(modal===null) return
+    event.preventDefault()
+    modal.classList.add("hidden");
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.close').removeEventListener('click', closeModal)
+    modal.querySelector('.modal-wrapper').removeEventListener('click', stopPropagation)
+    modal=null
+}
+
+const stopPropagation = function(event){
+    event.stopPropagation()
+}
+
+document.getElementById("js-modal1").addEventListener('click', openModal)
+document.getElementById("add-image").addEventListener('click', openModal)
+document.getElementById("modal-arrow-left1").addEventListener('click', openModal)
---
+++ views/login.js
@@ -0,0 +1,30 @@
+function API_login(event){
+    event.preventDefault();
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+    const data = { email, password };
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Succès:', data);
+        if (data.userId==1) {
+            localStorage.setItem('admin', '1');
+            localStorage.setItem('token', data.token);
+            window.location.href = '../index.html';
+        } 
+        else {
+            alert("combinaison login/mot de passe incorrecte");
+            localStorage.setItem('admin', '0');
+        }
+    })
+    .catch((error) => {
+        console.error('Erreur:', error);
+    });
+}
+
+document.getElementById('login_form').addEventListener('submit', API_login); 
